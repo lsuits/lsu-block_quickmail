5c89146ad0d2c9192079b50b6909986d1731e263
Barbara Vergara <dptoinformatica@enfermeriacyl.com>
2013-02-06 12:56:53 +0100

    New version for Quicmail
    Add new functions * Create folders
    * Management of email 
    * Email Search 
    * Replicate email in Moodle

--- block_quickmail.php
+++ block_quickmail.php
@@ -35,40 +35,61 @@ class block_quickmail extends block_list {
         $icon_class = array('class' => 'icon');
 
         if ($can_send) {
-            $cparam = array('courseid' => $COURSE->id);
-
-            $send_email_str = quickmail::_s('composenew');
-            $send_email = html_writer::link(
-                new moodle_url('/blocks/quickmail/email.php', $cparam),
-                $send_email_str
-            );
-            $this->content->items[] = $send_email;
-            $this->content->icons[] = $OUTPUT->pix_icon('i/email', $send_email_str, 'moodle', $icon_class);
-
-            $signature_str = quickmail::_s('signature');
-            $signature = html_writer::link(
-                new moodle_url('/blocks/quickmail/signature.php', $cparam),
-                $signature_str
-            );
-            $this->content->items[] = $signature;
-            $this->content->icons[] = $OUTPUT->pix_icon('i/edit', $signature_str, 'moodle', $icon_class);
-
-            $draft_params = $cparam + array('type' => 'drafts');
-            $drafts_email_str = quickmail::_s('drafts');
-            $drafts = html_writer::link(
-                new moodle_url('/blocks/quickmail/emaillog.php', $draft_params),
-                $drafts_email_str
-            );
-            $this->content->items[] = $drafts;
-            $this->content->icons[] = $OUTPUT->pix_icon('i/settings', $drafts_email_str, 'moodle', $icon_class);
-
-            $history_str = quickmail::_s('history');
-            $history = html_writer::link(
-                new moodle_url('/blocks/quickmail/emaillog.php', $cparam),
-                $history_str
-            );
-            $this->content->items[] = $history;
-            $this->content->icons[] = $OUTPUT->pix_icon('i/settings', $history_str, 'moodle', $icon_class);
+            
+          $cparam = array('courseid' => $COURSE->id);
+                        
+         $send_email_str = quickmail::_s('composenew');
+         $send_email = html_writer::link(
+         new moodle_url('/blocks/quickmail/email.php', $cparam),
+         $send_email_str
+         );
+         $this->content->items[] = $send_email;
+         $this->content->icons[] = $OUTPUT->pix_icon('i/email', $send_email_str, 'moodle', $icon_class);
+         
+         $signature_str = quickmail::_s('signature');
+         $signature = html_writer::link(
+         new moodle_url('/blocks/quickmail/signature.php', $cparam),
+         $signature_str
+         );
+         $this->content->items[] = $signature;
+         $this->content->icons[] = $OUTPUT->pix_icon('i/edit', $signature_str, 'moodle', $icon_class);
+         
+         
+         $recibidos_params = $cparam + array('action'=>'received','typeid'=>$USER->id,'type' =>'received');
+         $recibidos_email_str = quickmail::_s('received');
+         $recibidos = html_writer::link(
+         new moodle_url('/blocks/quickmail/emaillog.php', $recibidos_params),
+         $recibidos_email_str
+         );
+         $this->content->items[] = $recibidos;
+         $this->content->icons[] = $OUTPUT->pix_icon('i/settings', $recibidos_email_str, 'moodle', $icon_class);
+         
+         $otras= $DB->get_records('block_quickmail_folders', array ('id_curso'=>$COURSE->id, 'id_alumno'=>$USER->id), 'nombre ASC', 'nombre,id');
+         foreach ($otras as $carpeta) 
+         {
+         $ruta_otras_capertas=new moodle_url('/blocks/quickmail/emaillog.php', array('courseid' => $COURSE->id, 'action'=>'otrasCapetas','typeid'=>$USER->id,'id_folder' =>$carpeta->id, 'type'=>'folders_msg'));
+         
+         $this->content->items[] = html_writer::link($ruta_otras_capertas, $carpeta->nombre);
+         $this->content->icons[] = $OUTPUT->pix_icon('i/settings', $carpeta->nombre, 'moodle', $icon_class);
+         
+         }
+         $draft_params = $cparam + array('type' => 'drafts');
+         $drafts_email_str = quickmail::_s('drafts');
+         $drafts = html_writer::link(
+         new moodle_url('/blocks/quickmail/emaillog.php', $draft_params),
+         $drafts_email_str
+         );
+         $this->content->items[] = $drafts;
+         $this->content->icons[] = $OUTPUT->pix_icon('i/settings', $drafts_email_str, 'moodle', $icon_class);
+         
+         $history_str = quickmail::_s('history');
+         $history = html_writer::link(
+         new moodle_url('/blocks/quickmail/emaillog.php', $cparam),
+         $history_str
+         );
+         $this->content->items[] = $history;
+         $this->content->icons[] = $OUTPUT->pix_icon('i/settings', $history_str, 'moodle', $icon_class);
+         
         }
 
         if (has_capability('block/quickmail:allowalternate', $context)) {
