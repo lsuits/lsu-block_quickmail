12d8d48542dc114aab1cbbcaef04c5e68a142c59
Robert Russo <rrusso@lsu.edu>
2014-08-20 14:48:18 -0500

    Updated version

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
 // Written at Louisiana State University
-$plugin->version = 2014042914;
+$plugin->version = 2014082000;
 $plugin->requires = 2013051400;
 $plugin->release = "v1.5.0";
 $plugin->maturity = MATURITY_STABLE; 
