lsuits/lsu-block_quickmail

commit 70a772d0f0f0ea7a0c54b79138c5b8abd3238bba

corrected get_context to be context_XXXX, and fixed SQL to include additional moodle fullname requirements

backup/moodle2/restore_quickmail_stepslib.php
@@ -27,7 +27,7 @@ class restore_quickmail_log_structure_step extends restore_structure_step {
         if ($restore and isset($data->emaillogs['log'])) {
             global $DB;
 
-            $current = get_context_instance(CONTEXT_COURSE, $this->get_courseid());
+            $context = context_course::instance($this->get_courseid());
 
             $params = array(
                 'backupid' => $this->get_restoreid(),

alternate.php
@@ -12,8 +12,7 @@ $flash = optional_param('flash', 0, PARAM_INT);
 
 $course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
 
-$context = get_context_instance(CONTEXT_COURSE, $courseid);
-
+$context = context_course::instance($courseid);
 // Permission
 require_login($course);
 require_capability('block/quickmail:allowalternate', $context);

backup/moodle2/backup_quickmail_stepslib.php
@@ -5,8 +5,7 @@ class backup_quickmail_block_structure_step extends backup_block_structure_step 
         global $DB;
 
         $params = array('courseid' => $this->get_courseid());
-        $context = get_context_instance(CONTEXT_COURSE, $params['courseid']);
-
+        $context = context_course::instance( $params['courseid']);
         $quickmail_logs = $DB->get_records('block_quickmail_log', $params);
         $include_history = $this->get_setting_value('include_quickmail_log');
 

block_quickmail.php
@@ -35,7 +35,7 @@ class block_quickmail extends block_list {
         $this->content->icons = array();
         $this->content->footer = '';
 
-        $context = get_context_instance(CONTEXT_COURSE, $COURSE->id);
+        $context = context_course::instance($COURSE->id);
 
         $config = quickmail::load_config($COURSE->id);
         $permission = has_capability('block/quickmail:cansend', $context);

config.php
@@ -15,7 +15,7 @@ if (!$course = $DB->get_record('course', array('id' => $courseid))) {
     print_error('no_course', 'block_quickmail', '', $courseid);
 }
 
-$context= get_context_instance(CONTEXT_COURSE, $courseid);
+$context = context_course::instance($courseid);
 
 require_capability('block/quickmail:canconfig', $context);
 

db/upgradelib.php
@@ -31,7 +31,8 @@ function migrate_quickmail_20(){
 			$filename=$quickmail_log_record->attachment;
 		}
 		$fs = get_file_storage();
-		$coursecontext = get_context_instance(CONTEXT_COURSE,$quickmail_log_record->courseid);
+                $coursecontext = context_course::instance($quickmail_log_record->courseid);
+
 		$coursefile=$fs->get_file($coursecontext->id, 'course', 'legacy', 0, $filepath, $filename);
 		if($coursefile){
 			if($notrootfile){

email.php
@@ -46,7 +46,7 @@ if (!empty($type) and empty($typeid)) {
 
 $config = quickmail::load_config($courseid);
 
-$context = get_context_instance(CONTEXT_COURSE, $courseid);
+$context = context_course::instance($courseid);
 $has_permission = (
     has_capability('block/quickmail:cansend', $context) or
     !empty($config['allowstudents'])
@@ -313,11 +313,10 @@ if ($form->is_cancelled()) {
 
                 // create a temporary testing variable to make sure success always equals false
                 //@todo erase this business. 
-                $success = false;
+                //$success = false;
                 
                 if(!$success) {
                     $warnings[] = get_string("no_email", 'block_quickmail', $everyone[$userid]);
-                
                     $data->failuserids[] = $userid;
                     
                 }
@@ -359,12 +358,12 @@ echo $OUTPUT->header();
 echo $OUTPUT->heading($blockname);
 
 foreach ($warnings as $type => $warning) {
-    $class = ($type == 'success') ? 'notifysuccess' : 'notifyproblem';
+    $class = ($type === 'success') ? 'notifysuccess' : 'notifyproblem';
+    //$class = 'notifyproblem';
     echo $OUTPUT->notification($warning, $class);
 }
 
 echo html_writer::start_tag('div', array('class' => 'no-overflow'));
 $form->display();
 echo html_writer::end_tag('div');
-
 echo $OUTPUT->footer();

email_form.php
@@ -92,8 +92,8 @@ class email_form extends moodleform {
         $draft_link = html_writer::link ($gen_url('drafts'), quickmail::_s('drafts'));
         $links[] =& $mform->createElement('static', 'draft_link', '', $draft_link);
 
-        $context = get_context_instance(CONTEXT_COURSE, $COURSE->id);
-
+        $context = context_course::instance($COURSE->id);
+        
         $config = quickmail::load_config($COURSE->id);
 
         $can_send = (

commit c5d674e65c8dfd866fcceb3cd0d484e7db692fe8

fixed regression introduced in commit 70a772d0f0f0ea7a0c54b79138c5b8abd3238bba

backup/moodle2/restore_quickmail_stepslib.php
@@ -27,18 +27,18 @@ class restore_quickmail_log_structure_step extends restore_structure_step {
         if ($restore and isset($data->emaillogs['log'])) {
             global $DB;
 
-            $context = context_course::instance($this->get_courseid());
+            $current = context_course::instance($this->get_courseid());
 
             $params = array(
                 'backupid' => $this->get_restoreid(),
                 'itemname' => 'context',
-                'newitemid' => $current->id
+                'newitemid' => $context->id
             );
 
             $id = $DB->get_record('backup_ids_temp', $params)->itemid;
 
             foreach ($data->emaillogs['log'] as $log) {
-                $this->process_log($log, $id, $current);
+                $this->process_log($log, $id, $context);
             }
         }
     }