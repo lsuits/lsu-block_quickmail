038d26a87bab233496d644cc6f20ec5374d67125
David Elliott <elliott2081@gmail.com>
2014-10-28 13:49:55 -0500

    working - please test

--- settings.php
+++ settings.php
@@ -68,18 +68,6 @@ if($ADMIN->fulltree) {
         )
     );
 
-//    $download_auth_only = array(
-//        'download_auth_only' => get_string('download_auth_only', 'block_quickmail'),
-//        'download_open' => get_string('download_open', 'block_quickmail')
-//    );
-//
-//    $settings->add(
-//        new admin_setting_configselect('block_quickmail_downloads',
-//            quickmail::_s('downloads'), quickmail::_s('downloads_desc'),
-//            'download_auth_only', $download_auth_only
-//        )
-//    );
-
     $settings->add(
         new admin_setting_configcheckbox('block_quickmail_downloads',
             quickmail::_s('downloads'), quickmail::_s('downloads_desc'),
