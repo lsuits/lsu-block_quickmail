7d642632d6c618d157cbef237ab2fe4000a4117e
David Elliott <elliott2081@gmail.com>
2015-02-11 17:04:51 -0600

    added settings backup feature, still needs one change possibly, basically on restore even if you didn't select logs it'll ask if they should be restored, because both logs and settings are checking the same booleans, might fix with my an explanation, but probably try to figure out which root setting I can test

--- backup/moodle2/restore_quickmail_stepslib.php
+++ backup/moodle2/restore_quickmail_stepslib.php
@@ -7,6 +7,7 @@ class restore_quickmail_log_structure_step extends restore_structure_step {
         $paths[] = new restore_path_element('block', '/block', true);
         $paths[] = new restore_path_element('log', '/block/emaillogs/log');
 
+        $paths[] = new restore_path_element('block_level_setting', '/block/emaillogs/block_level_setting');
         return $paths;
     }
 
@@ -40,6 +41,25 @@ class restore_quickmail_log_structure_step extends restore_structure_step {
             foreach ($data->emaillogs['log'] as $log) {
                 $this->process_log($log, $id, $current);
             }
+        }
+
+        if(isset($data->emaillogs['block_level_setting'])){
+            foreach ($data->emaillogs['block_level_setting'] as $block_level_setting) {
+                $this->process_block_level_setting($block_level_setting, $this->get_courseid());
+            }
+        }
+    }
+
+
+    protected function process_block_level_setting($block_level_setting, $courseid) {
+        global $DB;
+        if($block_level_setting['name']){
+                //quickmail::default_config($courseid);
+                $config = new stdClass;
+                $config->coursesid = $courseid;
+                $config->name = $block_level_setting['name'];
+                $config->value = $block_level_setting['value'];
+                $DB->insert_record('block_quickmail_config', $config);
         }
     }
 
--- version.php
+++ version.php
@@ -1,7 +1,8 @@
 <?php
 
 // Written at Louisiana State University
-$plugin->version = 2015011410;
+//
+$plugin->version = 2015122912;
 $plugin->requires = 2013051400;
 $plugin->release = "v1.5.0";
 $plugin->maturity = MATURITY_STABLE; 
