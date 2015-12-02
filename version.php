8b0e921d1a829fb862fa8f82885c20174d65f2cf
Robert Russo <rrusso@lsu.edu>
2015-12-02 10:40:10 -0600

    Updated version for upgrades

--- version.php
+++ version.php
@@ -4,6 +4,6 @@
 //
 $plugin->version = 2015122912;
 $plugin->requires = 2013051400;
-$plugin->release = "v1.5.0";
+$plugin->release = "v1.5.5";
 $plugin->maturity = MATURITY_STABLE; 
 $plugin->component = 'block_quickmail';
