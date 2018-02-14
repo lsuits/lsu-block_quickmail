eed066a08648bb757169d94eac0469343f25da79
Chad Mazilly <chadmazilly@gmail.com>
2018-02-14 12:46:03 -0600

    switch cache to application

--- db/caches.php
+++ db/caches.php
@@ -2,22 +2,22 @@
 
 $definitions = [
     'qm_msg_recip_count' => [
-        'mode' => cache_store::MODE_SESSION,
+        'mode' => cache_store::MODE_APPLICATION,
         'simplekeys' => true,
         'simpledata' => true,
     ],
     'qm_msg_addl_email_count' => [
-        'mode' => cache_store::MODE_SESSION,
+        'mode' => cache_store::MODE_APPLICATION,
         'simplekeys' => true,
         'simpledata' => true,
     ],
     'qm_msg_attach_count' => [
-        'mode' => cache_store::MODE_SESSION,
+        'mode' => cache_store::MODE_APPLICATION,
         'simplekeys' => true,
         'simpledata' => true,
     ],
     'qm_msg_deliv_count' => [
-        'mode' => cache_store::MODE_SESSION,
+        'mode' => cache_store::MODE_APPLICATION,
         'simplekeys' => true,
         'simpledata' => true,
     ]
