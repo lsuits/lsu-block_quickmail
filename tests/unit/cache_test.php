1929bb3db21c8930ee9e4aaa519cc4dbaf028db6
Chad Mazilly <chadmazilly@gmail.com>
2018-03-12 16:06:53 -0500

    remove unnecessary class declaration

--- tests/unit/cache_test.php
+++ tests/unit/cache_test.php
@@ -2,8 +2,6 @@
  
 require_once(dirname(__FILE__) . '/traits/unit_testcase_traits.php');
 
-use block_quickmail_cache;
-
 class block_quickmail_cache_testcase extends advanced_testcase {
     
     use has_general_helpers;
