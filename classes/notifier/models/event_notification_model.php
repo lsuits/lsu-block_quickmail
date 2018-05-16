646c11db1134e974a3a1023a3d143a4819399358
Chad Mazilly <chadmazilly@gmail.com>
2018-05-16 11:20:12 -0500

    and this

--- classes/notifier/models/event_notification_model.php
+++ classes/notifier/models/event_notification_model.php
@@ -4,13 +4,12 @@ namespace block_quickmail\notifier\models;
 
 use block_quickmail\notifier\models\interfaces\notification_model_interface;
 use block_quickmail\notifier\models\notification_model;
-use block_quickmail\persistents\notification;
 use block_quickmail\persistents\interfaces\notification_type_interface;
 
 abstract class event_notification_model extends notification_model implements notification_model_interface {
 
-    public function __construct(notification_type_interface $notification_type_interface, notification $notification) {
-        parent::__construct($notification_type_interface, $notification);
+    public function __construct(notification_type_interface $notification_type_interface) {
+        parent::__construct($notification_type_interface);
     }
 
 }
--- classes/notifier/models/notification_model.php
+++ classes/notifier/models/notification_model.php
@@ -4,38 +4,15 @@ namespace block_quickmail\notifier\models;
 
 use block_quickmail\notifier\models\interfaces\notification_model_interface;
 use block_quickmail\persistents\interfaces\notification_type_interface;
-use block_quickmail\persistents\notification;
 
 abstract class notification_model implements notification_model_interface {
 
     public $notification_type_interface;
     public $notification;
 
-    public function __construct(notification_type_interface $notification_type_interface, notification $notification) {
+    public function __construct(notification_type_interface $notification_type_interface) {
         $this->notification_type_interface = $notification_type_interface;
-        $this->notification = $notification;
-    }
-
-    /**
-     * Returns a fully namespaced notification_model class name from the given notification_type_interface
-     * 
-     * @param  notification_type_interface  $notification_type_interface
-     * @return string
-     */
-    protected static function get_full_model_class_name($notification_type_interface)
-    {
-        return 'block_quickmail\notifier\models\\' . $notification_type_interface::$notification_type_key . '\\' . self::get_model_class_name($notification_type_interface->get('model'));
-    }
-
-    /**
-     * Returns a notification model class name given a notification_type_interface's model
-     * 
-     * @param  string  $model
-     * @return string
-     */
-    protected static function get_model_class_name($model)
-    {
-        return str_replace('-', '_', $model) . '_model';
+        $this->notification = $notification_type_interface->get_notification();
     }
 
     /**
@@ -46,11 +23,31 @@ abstract class notification_model implements notification_model_interface {
      */
     public static function make(notification_type_interface $notification_type_interface)
     {
-        $notification = $notification_type_interface->get_notification();
-
         $class = static::get_full_model_class_name($notification_type_interface);
 
-        return new $class($notification_type_interface, $notification);
+        return new $class($notification_type_interface);
+    }
+
+    /**
+     * Returns a fully namespaced notification_model class name from the given notification_type_interface
+     * 
+     * @param  notification_type_interface  $notification_type_interface
+     * @return string
+     */
+    public static function get_full_model_class_name($notification_type_interface)
+    {
+        return 'block_quickmail\notifier\models\\' . $notification_type_interface::$notification_type_key . '\\' . self::get_model_class_name($notification_type_interface->get('model'));
+    }
+
+    /**
+     * Returns a notification model class name given a notification_type_interface's model
+     * 
+     * @param  string  $model
+     * @return string
+     */
+    public static function get_model_class_name($model)
+    {
+        return str_replace('-', '_', $model) . '_model';
     }
 
 }
