61ad0efbe13f37e5e74f003f0031bb22cbc10f8d
Chad Mazilly <chadmazilly@gmail.com>
2018-05-16 11:11:19 -0500

    forgot this one

--- classes/notifier/models/event_notification_model.php
+++ classes/notifier/models/event_notification_model.php
@@ -9,12 +9,8 @@ use block_quickmail\persistents\interfaces\notification_type_interface;
 
 abstract class event_notification_model extends notification_model implements notification_model_interface {
 
-    public $notification_type_interface;
-    public $notification;
-
     public function __construct(notification_type_interface $notification_type_interface, notification $notification) {
-        $this->notification_type_interface = $notification_type_interface;
-        $this->notification = $notification;
+        parent::__construct($notification_type_interface, $notification);
     }
 
 }
